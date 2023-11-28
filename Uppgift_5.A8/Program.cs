c95c5b6097de821c34dad5b2b0b177200a483a55
ArvidUnell <arun06001@utb.vaxjo.se>
2023-11-28 09:09:04 +0100

    Add project files.

---
+++ Uppgift_5.A8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_5_A8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] talArr = { 25, 68, 240, 398, 13393 };
+            
+            foreach (int tal in talArr)
+            {
+                for (int i = 0; i < tal.ToString().Length; i++)
+                {
+                    Console.Write(tal.ToString()[i]);
+                    Task.Delay(500).Wait();
+                }
+                Task.Delay(1000).Wait();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
